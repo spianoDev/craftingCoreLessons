4fd481317f8901c5a07cd0fa1dc6867166c36a2c
spiano <lafleurpiano@gmail.com>
2019-11-18 23:03:52 -0600

    Updates

---
+++ precache-manifest.ad4eaa904ef6fa415252e0dfc54e3468.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42760742fc56fcdbf7177e9951305430",
+    "url": "/craftingcorelessons/index.html"
+  },
+  {
+    "revision": "b39dc32fe506897b3b9d",
+    "url": "/craftingcorelessons/static/css/main.aa0dadcc.chunk.css"
+  },
+  {
+    "revision": "f810c94883363d30c617",
+    "url": "/craftingcorelessons/static/js/2.21326489.chunk.js"
+  },
+  {
+    "revision": "b39dc32fe506897b3b9d",
+    "url": "/craftingcorelessons/static/js/main.04fa30c9.chunk.js"
+  },
+  {
+    "revision": "11d6c6d596239a42a66a",
+    "url": "/craftingcorelessons/static/js/runtime-main.083cfd00.js"
+  }
+]);
